e99068556068bb7e8a80631cd4fd5131f0be0a44
Bhavuk001 <bhavi9mehta@gmail.com>
2025-08-16 03:37:08 +0530

    Made loader successfully

---
+++ final.js
@@ -0,0 +1,40 @@
+var tl = gsap.timeline()
+tl.from(".line h1",{
+    y:150,
+    stagger: 0.2,
+    duration: 0.4,
+    delay:0.5,
+})
+tl.from("#line1-part1",{
+ opacity: 0,
+ onStart: function(){
+     var timer = document.querySelector("#line1-part1 h5");
+     var grow = 0;
+     setInterval(function(){
+    if(grow<100){
+        grow++
+        timer.innerHTML = grow;
+    }else{
+        timer.innerHTML = grow;
+    }
+},30)}
+})
+tl.to(".line h2",{
+    animationName:"anime",
+    opacity:1,
+})
+tl.to("#loader",{
+    opacity: 0,
+    duration: 0.3,
+    delay: 3.5,
+})
+tl.from("#page1",{
+    y:1200,         //issue
+    opacity:1,
+    delay:0.2,
+    duration:0.5,
+    ease:Power3,
+})
+tl.to("#loader",{
+    display:none,
+})
