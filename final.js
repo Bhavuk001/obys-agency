8fc50bb3df4c227aaf5fa45120cc608ba9b10484
Bhavuk001 <bhavi9mehta@gmail.com>
2025-08-17 01:35:35 +0530

    page1 done

--- final.js
+++ final.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline()
+function loadigAnimation () {
+    var tl = gsap.timeline()
 tl.from(".line h1",{
     y:150,
     stagger: 0.2,
@@ -29,12 +30,34 @@ tl.to("#loader",{
     delay: 3.5,
 })
 tl.from("#page1",{
-    y:1200,         //issue
+    y:1200,
     opacity:1,
     delay:0.2,
-    duration:0.5,
+    duration:0.4,
     ease:Power3,
 })
-tl.to("#loader",{
-    display:none,
+tl.set("#loader",{    // added own...
+    display:"none",
+    pointerEvents:"none",
+});
+tl.from("#nav",{
+    opacity:0,
 })
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:100,
+    stagger:0.2,
+})
+}
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(circle){
+        gsap.to("#cursor",{
+            left:circle.x,
+            top:circle.y
+        })
+    })
+    Shery.makeMagnet("#nav-part2 h4");
+}
+
+loadigAnimation();
+cursorAnimation();
